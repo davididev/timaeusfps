846abb85a1b459eef26a41d12b3abb804b11b00c
davididev <davidwaggener@gmail.com>
2026-05-14 23:48:10 -0400

    First post (init)

---
+++ Assets/Magic Light Probes/MLPPrefab.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MagicLightProbes
+{
+    [ExecuteInEditMode]
+    public class MLPPrefab : MonoBehaviour
+    {
+        public new string name;
+        public string uid;
+
+        private void OnEnable()
+        {
+            name = gameObject.name;
+            uid = gameObject.GetInstanceID().ToString();
+        }
+    }
+}
